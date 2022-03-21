c3482e3439e58cb8fa507561a4960348eaa0a702
balaji1610 <balajiece1610@gmail.com>
2022-03-21 18:35:26 +0530

    ProPerty Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  fetchData = false;
+constructor() {
+setTimeout(()=>{
+this.fetchData = true;
+},5000)
+
+} // Property Binding
 
   ngOnInit(): void {
   }
